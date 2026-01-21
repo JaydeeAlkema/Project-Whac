e5412a246db5b7845a62be53a396c7e40b5e7039
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2026-01-21 17:28:05 +0100

    Toggle between correct ui screens

--- Assets/Scripts/UI/GameScreen/GameScreenDriver.cs
+++ Assets/Scripts/UI/GameScreen/GameScreenDriver.cs
@@ -1,4 +1,6 @@
-﻿using Core.GameComposition;
+﻿using System;
+using Core.GameComposition;
+using EventArgs.Timer;
 using UI.GameScreen.Score;
 using UI.GameScreen.Timer;
 using UnityEngine;
@@ -11,21 +13,55 @@ namespace UI.GameScreen
 		[SerializeField] private UIDocument _ui;
 		[SerializeField] private GameCompositionRoot _compositionRoot;
 
+		private VisualElement _game;
+		private VisualElement _gameOver;
+
 		private ScoreScope _scoreScope;
 		private TimerScope _timerScope;
+
+		private IDisposable _timerEndedDisposable;
 
 		private void Start()
 		{
 			VisualElement root = _ui.rootVisualElement;
+
 			_scoreScope = new(_compositionRoot.ScoreService, root);
 			_timerScope = new(_compositionRoot.TimerService, root);
-		}
 
+			CacheVisualElements(root);
+			ToggleGameScreen();
+
+			_timerEndedDisposable = _compositionRoot.EventBus.Subscribe<TimerEndedEventArgs>(OnTimerEnded);
+		}
 
 		private void OnDestroy()
 		{
 			_scoreScope?.Dispose();
 			_timerScope?.Dispose();
+			_timerEndedDisposable?.Dispose();
+		}
+
+		private void OnTimerEnded(TimerEndedEventArgs _)
+		{
+			ToggleGameOverScreen();
+		}
+
+		private void CacheVisualElements(VisualElement root)
+		{
+			_game = root.Q<VisualElement>("game");
+			_gameOver = root.Q<VisualElement>("gameOver");
+		}
+
+		private void ToggleGameScreen()
+		{
+			_game.EnableInClassList("hidden", false);
+			_gameOver.EnableInClassList("hidden", true);
+		}
+
+		private void ToggleGameOverScreen()
+		{
+			_game.EnableInClassList("hidden", true);
+			_gameOver.EnableInClassList("hidden", false);
 		}
 	}
 }
